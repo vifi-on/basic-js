2c7424de1ee340f940c02f1a2d54752fd506acec
vifi-on <ladiasman1992@gmail.com>
2021-02-23 16:26:09 +0300

    feat: add function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const [
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+  ] = Object.values(options)
+
+  const subArrOfAdd = new Array(additionRepeatTimes).fill(String(addition))
+  const strOfAdd = `${str}${subArrOfAdd.join(String(additionSeparator))}`
+  const arrOfAdd = new Array(repeatTimes).fill(strOfAdd)
+  const result = arrOfAdd.join(String(separator))
+
+  return result
 };
   

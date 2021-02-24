72c8b2c7873b251f75ff54d25dfb2a617ac97122
vifi-on <ladiasman1992@gmail.com>
2021-02-24 18:41:34 +0300

    feat: add function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,76 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(type = true) {
+    this.type = type
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+    this.alphLen = this.alphabet.length
+    this.repeatStr = (key, str) => {
+      const strLen = str.length
+      const keyLen = key.length
+      let newStr = ''
+      
+      for (let i = 0, j = 0; i < strLen; i++) {
+        if (i % keyLen === 0) {
+          j = 0
+        }
+        newStr += key[j]
+        j++
+      }
+      
+      return newStr
+    }
+  }
+
+  encrypt(str, key) {
+    const strLen = str.length
+    let result
+    const output = []
+
+    str = str.toUpperCase()
+    key = key.toUpperCase()
+    const newKey = this.repeatStr(key, str).split('')
+
+    for (let i = 0; i < strLen; i++) {
+      if (this.alphabet.includes(str[i])) {
+
+        result = (this.alphabet.indexOf(str[i]) +
+          this.alphabet.indexOf(newKey.shift())
+        ) % this.alphLen
+
+        output.push(this.alphabet[result])
+      } else {
+        output.push(str[i])
+      }
+    }
+
+    return this.type ? output.join('') : output.reverse().join('')
+  }
+
+  decrypt(str, key) {
+    const strLen = str.length
+    let result
+    const output = []
+
+    str = str.toUpperCase()
+    key = key.toUpperCase()
+    const newKey = this.repeatStr(key, str).split('')
+
+    for (let i = 0; i < strLen; i++) {
+      if (this.alphabet.includes(str[i])) {
+
+        result = (this.alphabet.indexOf(str[i]) + this.alphLen -
+          this.alphabet.indexOf(newKey.shift())
+        ) % this.alphLen
+
+        output.push(this.alphabet[result])
+      } else {
+        output.push(str[i])
+      }
+    }
+
+    return this.type ? output.join('') : output.reverse().join('')
   }
 }
 
